49d8b65faaf28b6a0456e402e0d99a59fc8fbf05
Hiso <dead.inside.me@gmail.com>
2023-01-04 18:40:26 -0300

    Primer metodo de insersion masiva

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
@@ -7,16 +10,36 @@ export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
+
+
+
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    await this.pokemonModel.deleteMany({}); // delete * from pokemons
+
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+    const insertPromisesArray = [];
 
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[ segments.length - 2 ];
 
-      console.log({name, no})
-    })
-    return data.results;
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      insertPromisesArray.push(
+        this.pokemonModel.create({ name, no })
+      );
+
+    });
+
+    await Promise.all( insertPromisesArray );
+
+    return "Seed Executed";
   }
 
 }
